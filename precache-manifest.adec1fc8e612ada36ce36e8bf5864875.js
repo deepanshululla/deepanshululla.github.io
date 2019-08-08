bbce9779e9dafe85c0c6349f5a1eaa10b5bf6141
deepanshululla <deepanshu.lulla@gmail.com>
2019-08-07 23:02:48 -0400

    Updates

--- precache-manifest.adec1fc8e612ada36ce36e8bf5864875.js
+++ precache-manifest.adec1fc8e612ada36ce36e8bf5864875.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "64407dd28381192fc00737bceb2cb815",
+    "revision": "95c84ffde5dc2d6ae7f27316f773213e",
     "url": "/index.html"
   },
   {
-    "revision": "b2ae245847d0056b9674",
+    "revision": "76889692adbe674c7d85",
     "url": "/static/css/main.87d99f42.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.9ea7a6e4.chunk.js"
   },
   {
-    "revision": "b2ae245847d0056b9674",
-    "url": "/static/js/main.37f0be9f.chunk.js"
+    "revision": "76889692adbe674c7d85",
+    "url": "/static/js/main.35b17c00.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
