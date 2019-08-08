771aab2d795156cef2b0b64693f384142afff049
deepanshululla <deepanshu.lulla@gmail.com>
2019-08-07 23:52:48 -0400

    build

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import Modal from 'react-modal';
 
 
-// Modal.setAppElement('#react-container')
+Modal.setAppElement('#react-container')
 
 
 ReactDOM.render(<App />, document.getElementById('react-container'));
