08b3595d8086bd727f15fc88bcd83ae474d72108
deepanshululla <deepanshu.lulla@gmail.com>
2019-08-07 23:12:47 -0400

    build

---
+++ precache-manifest.ae501706ca32d02f7ee633b0251ae29a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44263cde9ffa7a43e48414b2921fdfa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97d9ad4814bcdff97aa9",
+    "url": "/static/css/main.87d99f42.chunk.css"
+  },
+  {
+    "revision": "8d71609689930b6c080c",
+    "url": "/static/js/2.9ea7a6e4.chunk.js"
+  },
+  {
+    "revision": "97d9ad4814bcdff97aa9",
+    "url": "/static/js/main.ea3f4030.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -2,7 +2,7 @@ import React, {Component } from 'react';
 
 class ProjectOverlay extends Component {
     render(){
-        var project = this.props.data.projectItem;
+        var project = this.props.projectItem;
         console.log(this.props);
         return (<div className="portfolio-item-meta">
                 <h5>{project.title}</h5>
