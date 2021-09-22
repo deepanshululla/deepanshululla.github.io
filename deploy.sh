3042c90b50fcf3db759b163ced3c54bfaf81e558
deepanshululla <deepanshu.lulla@gmail.com>
2021-09-22 10:32:36 -0400

    build

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 rm -rf node_modules
 rm package-lock.json
 npm install
-npm run deploy;
-cp -aR ./build/* ./;
-git add .;
-git commit -am "build";
-git push -f origin master;
+npm run deploy
+cp -aR ./build/* ./
+git add .
+git commit -am "build"
+git push -f origin master
